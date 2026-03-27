e0f46bc62aaaa6061af61379a39c0afbcfc9ffa3
Infinitemarcus <marcus.natrielli@outlook.com>
2026-03-27 00:56:19 -0300

    build: changes to build app .exe

--- src/renderer/components/Logo/index.tsx
+++ src/renderer/components/Logo/index.tsx
@@ -1,4 +1,5 @@
 import { Box, type SxProps, type Theme } from '@mui/material';
+import logoSrc from '../../public/logo.png';
 
 type Props = {
   sx?: SxProps<Theme>;
@@ -8,7 +9,7 @@ export const Logo = ({ sx }: Props) => {
   return (
     <Box
       component="img"
-      src="/logo.png"
+      src={logoSrc}
       alt="Logo do Projeto Hanna"
       sx={{
         width: 'clamp(240px, 24vw, 350px)',
